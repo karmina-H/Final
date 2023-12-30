1bbcc6b4855d6428315ce58355844463d8ad22fb
이현율 <gusdbf1114@gmail.com>
2023-12-30 22:30:40 +0900

    first

---
+++ src/test/java/com/example/final_git/FinalGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.final_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
